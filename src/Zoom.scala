f5976e53f7266ff453aea8443a4d9e2a469b9ff8
oetzi <oetzi101@gmail.com>
2011-06-20 00:06:56 +0100

    No longer breaks if user enters incorrect command

--- src/Zoom.scala
+++ src/Zoom.scala
@@ -9,6 +9,8 @@ object Zoom
 			case "next" => data = Track ask_for_race
 			case "results" => data = Track ask_for_results
 			//case "standings" => data = Track ask_for_standings
+			
+			case _ => data = List("'" + args(0) + "' is not a valid zoom command!")
 		} 
 		
 		Screen display data
