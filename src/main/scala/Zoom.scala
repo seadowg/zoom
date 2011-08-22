4048c9dad3c8efb3b7288544844c2a33dc8b2517
Callum Stott <oetzi101@gmail.com>
2011-08-22 23:45:23 +0200

    Edited src/main/scala/Zoom.scala via GitHub

--- src/main/scala/Zoom.scala
+++ src/main/scala/Zoom.scala
@@ -2,15 +2,12 @@ object Zoom
 {
 	def main(args : Array[String]) =
 	{	
-		var data : List[String] = null
-		
-	 	args(0) match 
-		{
-			case "next" => data = Track ask_for_race
-			case "results" => data = Track ask_for_results
+		var data : List[String] = args(0) match {
+			case "next" => Track ask_for_race
+			case "results" => Track ask_for_results
 			//case "standings" => data = Track ask_for_standings
 			
-			case _ => data = List("'" + args(0) + "' is not a valid zoom command!")
+			case _ => List("'" + args(0) + "' is not a valid zoom command!")
 		} 
 		
 		Screen display data
